ad020634f26800590db83d9e685958b3d8775e56
Vijay Patel <vijaypatel114200@gmail.com>
2025-02-23 10:20:07 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import { app,server } from "./socket/socket.js";
 dotenv.config({});
 
  
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 // middleware
 app.use(express.urlencoded({extended:true}));
